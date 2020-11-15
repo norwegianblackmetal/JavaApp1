97a303e21d03bf0559de83310cabdeab7e416dd0
norwegianblackmetal <finojenok.m@gmail.com>
2020-11-15 14:48:22 +0300

    add static files

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask import render_template
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
-@cross_origin()
 def index():
     return render_template('index.html')
 
